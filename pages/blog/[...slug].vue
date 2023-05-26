63fb6eb3a439d4883e3a4a454faccf8f0e0e611f
John Slowik <john@jslowik.com>
2023-05-26 12:34:41 -0500

    External links w/ blank target noted

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -3,4 +3,17 @@
     <ContentDoc />
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
+</script>
+<style lang="scss">
+a[target="_blank"] {
+  &:after {
+    content: url("/img/external.svg");
+    display: inline-block;
+    width: 15px;
+    height: 15px;
+    margin-right: 5px;
+  }
+}
+</style>
