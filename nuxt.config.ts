7fb03217f82e20633e94e93b91aead01ecdd9c61
John Slowik <john@jslowik.com>
2025-12-18 16:35:13 -0600

    Formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-12-18',
-  ssr: true,
-  modules: ["@nuxt/content"],
-  css: ["~/assets/css/main.scss"],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+	compatibilityDate: "2025-12-18",
+	ssr: true,
+	modules: ["@nuxt/content"],
+	css: ["~/assets/css/main.scss"],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
 });
