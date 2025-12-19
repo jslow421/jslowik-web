7e2655d691e88490835cf61b0b0f7eb56e4b52c6
John Slowik <john@jslowik.com>
2025-12-19 09:49:18 -0600

    Credly badge fix

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,14 @@
   <div class="prose prose-invert">
     <ContentRenderer v-if="page" :value="page" />
   </div>
-  <div
-    data-iframe-width="150"
-    data-iframe-height="270"
-    data-share-badge-id="84b32074-d0f9-422d-a472-92b8af0c82fb"
-    data-share-badge-host="https://www.credly.com"
-  ></div>
+  <div class="mt-8 bg-white p-4 rounded-lg inline-block">
+    <div
+      data-iframe-width="150"
+      data-iframe-height="270"
+      data-share-badge-id="84b32074-d0f9-422d-a472-92b8af0c82fb"
+      data-share-badge-host="https://www.credly.com"
+    ></div>
+  </div>
 </template>
 <script setup lang="ts">
 const route = useRoute();
