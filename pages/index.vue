60c3843c939bfb062204b7a92efa973d97822914
John Slowik <john@jslowik.com>
2025-12-19 10:01:07 -0600

    Flip to secondary

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,25 @@
 <template>
-  <div class="prose prose-invert">
-    <ContentRenderer v-if="page" :value="page" />
-  </div>
-  <div class="mt-8 bg-white p-4 rounded-lg inline-block">
-    <div
-      data-iframe-width="150"
-      data-iframe-height="270"
-      data-share-badge-id="84b32074-d0f9-422d-a472-92b8af0c82fb"
-      data-share-badge-host="https://www.credly.com"
-    ></div>
-  </div>
+	<div class="prose prose-invert">
+		<ContentRenderer v-if="page" :value="page" />
+	</div>
+	<div class="mt-8 bg-secondary p-4 rounded-lg inline-block">
+		<div
+			data-iframe-width="150"
+			data-iframe-height="270"
+			data-share-badge-id="84b32074-d0f9-422d-a472-92b8af0c82fb"
+			data-share-badge-host="https://www.credly.com"
+		></div>
+	</div>
 </template>
 <script setup lang="ts">
 const route = useRoute();
 const { data: page } = await useAsyncData(route.path, () => {
-  return queryCollection("content").path(route.path).first();
+	return queryCollection("content").path(route.path).first();
 });
 
 onMounted(async () => {
-  let credlyScript = document.createElement("script");
-  credlyScript.setAttribute(
-    "src",
-    "https://cdn.credly.com/assets/utilities/embed.js"
-  );
-  document.head.appendChild(credlyScript);
+	let credlyScript = document.createElement("script");
+	credlyScript.setAttribute("src", "https://cdn.credly.com/assets/utilities/embed.js");
+	document.head.appendChild(credlyScript);
 });
 </script>
